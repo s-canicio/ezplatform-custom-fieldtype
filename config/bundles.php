ed0f2ff47fb98780c73b3c3b07dc78398c1e983a
Canicio Sacha <s.canicio@inforca.mc>
2024-01-18 10:19:16 +0100

    Add debug

--- config/bundles.php
+++ config/bundles.php
@@ -88,4 +88,6 @@ return [
     KnpU\OAuth2ClientBundle\KnpUOAuth2ClientBundle::class => ['all' => true],
     Ibexa\Platform\Bundle\Migration\IbexaPlatformMigrationBundle::class => ['all' => true],
     Gregwar\CaptchaBundle\GregwarCaptchaBundle::class => ['all' => true],
+    Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
